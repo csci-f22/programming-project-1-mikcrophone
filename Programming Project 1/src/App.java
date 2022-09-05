9f2a8d9e3fdda17b8bcd1ea680736609d33d6b55
mikcrophone <69864260+mikcrophone@users.noreply.github.com>
2022-09-05 01:56:31 -0400

    Map
    Creating hashmap for keys.

--- Programming Project 1/src/App.java
+++ Programming Project 1/src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        //Scanner keyboard = new Scanner(System.in);  
+        //System.out.println("Enter Ciphertext: ");
+
+        //String ciphertext = keyboard.nextLine(); 
+
+        String string = "I am really busy right now";
+
+        String deciphertext = string.replaceAll("I", "l");
+
+        System.out.println(deciphertext);
+        
 }
+    }
+
